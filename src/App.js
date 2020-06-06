687483d969e5b8331967a46283a9f8b40d6e85a6
Hassan <hassan2935161@gmail.com>
2020-06-06 23:48:44 +0500

    files added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Route,Switch,Link} from 'react-router-dom'
+import Home from './components/Home';
+import Cart from './components/Cart';
+import Aboutus from './components/Aboutus';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <div>
+<nav>
+
+<ul className="navbar navbar-light text-danger bg-light"  id="Nav">
+<h3>Shop</h3>
+
+<li><Link to = "/home">Home</Link></li>
+<li><Link to = "/Aboutus">About us</Link></li>
+<li><i class="fa fa-shopping-cart" aria-hidden="true"><Link to = "/Cart">Cart</Link></i></li>
+</ul>
+
+</nav>
+<Switch>
+  <Route path="/home" exact> <Home></Home></Route>
+  
+  <Route path="/Aboutus"> <Aboutus></Aboutus></Route>
+  <Route path="/Cart"><Cart></Cart> </Route>
+</Switch>
+</div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Aboutus.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Aboutus(props) {
+    return (
+        <div>
+            i am in about us page
+            
+        </div>
+    )
+}
+export default Aboutus;
+
